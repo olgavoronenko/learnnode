2e52c73dd067838bdf20932e336eecf7fa2ccde5
Olga Voronenko <olga.voronenko@tptlive.ee>
2024-09-23 16:25:55 +0300

    Add html webpack pugin

--- index.js
+++
@@ -1,3 +0,0 @@
-//const fs = require('fs');
-import fs from 'fs';
-fs.writeFileSync('log.txt', 'Hello File!!!\n');
---
+++ src/es.js
@@ -0,0 +1,5 @@
+export default {
+    helloES(){
+        console.log("Hello ES");
+    }
+}
